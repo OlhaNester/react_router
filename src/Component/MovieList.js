d82bb5c874e98a432d7365dd78624d1c886c91e9
OlhaNester <olga_nv@ukr.net>
2021-11-01 22:07:29 +0200

    999

--- src/Component/MovieList.js
+++ src/Component/MovieList.js
@@ -2,9 +2,8 @@ import React from "react";
 
 import { NavLink, withRouter } from "react-router-dom";
 import MoviePreview from "./MoviePreview";
-import styles from './MovieList.module.css';
-import PropTypes from 'prop-types';
-import defaultMovie from './defaultMovie.jpg';
+import styles from "./MovieList.module.css";
+import PropTypes from "prop-types";
 
 const MovieList = ({ movies, location }) => {
   return (
@@ -17,22 +16,17 @@ const MovieList = ({ movies, location }) => {
               state: { from: location },
             }}
           >
-            {(movie.poster_path)? (<MoviePreview
+            <MoviePreview
               poster={movie.poster_path}
               title={movie.title}
               vote={movie.vote_average}
-            />):(<MoviePreview
-              poster={defaultMovie}
-              title={movie.title}
-              vote={movie.vote_average}
-            />)}
+            />
           </NavLink>
         </li>
       ))}
     </ul>
   );
 };
-
 
 // MovieList.PropTypes = {
 //   poster: PropTypes.string,
--- src/Component/MoviePreview.js
+++ src/Component/MoviePreview.js
@@ -1,12 +1,18 @@
 import React from "react";
+import defaultMovie from "../Component/defaultMovie.jpg";
+
 const MoviePreview = ({ poster, title, vote }) => {
   return (
     <div>
-      <img
-        src={`https://image.tmdb.org/t/p/w185${poster}`}
-        width="200"
-        alt=""
-      />
+      {poster !== null ? (
+        <img
+          src={`https://image.tmdb.org/t/p/w185${poster}`}
+          width="200"
+          alt=""
+        />
+      ) : (
+        <img src={defaultMovie} width="200" alt="" />
+      )}
       <h5>{title}</h5>
       <p>{vote}</p>
     </div>
